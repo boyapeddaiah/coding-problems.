5c9da07f4cf93b3da7cd10e249acf1c98042cfc4
Boya Peddaiah <boyapeddaiah1840@gmail.com>
2026-03-12 11:54:36 +0530

    Add files via upload

---
+++ basic programs/Arrays2D.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+
+/*class Arrays2D{
+    public static void main(String[]args){
+        int [][]arr=new int[2][2]; // 2/2 matrix..............
+        arr[0][0]=1;
+       arr[0][1]=2;
+        arr[1][0]=3;
+        arr[1][1]=4;
+        System.out.println(Arrays.deepToString(arr));
+    }
+}*/
+
+/*class Arrays2D{
+    public static void main(String[]args){
+        int [][] a=new int[3][];
+        a[0]=new int[4];
+        a[1]=new int[4];
+        a[2]=new int[4];
+        System.out.println(Arrays.deepToString(a));
+    }
+}*/
+
+
+
+/*class Arrays2D{
+    public static void main(String[]args){
+        int [][] numbers=new int[3][];
+        numbers[0]=new int[2];
+        numbers[1]=new int[4];
+        numbers[2]=new int[3];
+        System.out.println(Arrays.deepToString(numbers));
+        numbers[0][0]=1;
+        numbers[0][1]=2;
+        numbers[1][0]=3;
+        numbers[1][1]=4;
+        numbers[1][2]=5;
+        numbers[1][3]=6;
+        numbers[2][0]=7;
+        numbers[2][1]=8;
+        numbers[2][2]=9;
+        System.out.println(Arrays.deepToString(numbers));
+    }
+}*/
+
+/*class Arrays2D{
+    public static void main(String[]args){
+        int [][] arr=new int[3][3];
+        arr[0][0]=1;
+        //arr[0]=new int[4];
+        System.out.println(Arrays.deepToString(arr));
+    }
+}*/
+/*class Arrays2D{
+    public static void main(String[]args){
+        int [][] ar=new int[3][3];    //=====    3/3 matrix...
+        System.out.println(Arrays.deepToString(ar));
+
+    }
+}*/
+
+
+/*class Arrays2D{
+    public static void main(String[]args){
+        int [] a=new int[10];
+        System.out.println(Arrays.toString(a));
+    }
+} */
+
+    /*class Arrays2D{
+        public static void main(String[]args){
+            int[]a=new int[10];
+            a[0]=1;
+            a[7]=8;
+            System.out.println(Arrays.toString(a));
+        }
+    }
+*/
+class Arrays2D{
+    public static void main(String[]args){
+        int []a=new int[10];
+        a[8]=19;
+        a=new int[20];
+        a[10]=20;
+       // a={1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(a));
+    }
+}
