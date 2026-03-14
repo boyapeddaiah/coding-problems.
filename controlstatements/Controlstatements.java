e5f183dff6254985a70fa9d3851350b790fff779
Boya Peddaiah <boyapeddaiah1840@gmail.com>
2026-03-14 07:56:41 +0530

    Add files via upload

---
+++ controlstatements/Controlstatements.java
@@ -0,0 +1,14 @@
+class Controlstatements{
+    public static void main(String[]args){
+        int number1=40;
+        int number2=40;
+        //if (number1==number2)
+        if(number1.equals(number2))
+            {
+            System.out.println("true");
+        }
+        else{
+            System.out.println("flase");
+        }
+    }
+}
